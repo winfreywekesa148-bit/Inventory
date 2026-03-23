bf23319b81169c3f9f7d99e7003509b6dc7a0323
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-03-23 19:24:42 +0300

    Adds products

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -7,3 +7,10 @@ const products = [
 function logFirstProduct(products) {
     console.log(products[0]);
 };
+
+// addProduct
+function addProduct(products, product) {
+    products.push(product); // adds a new product to the array
+}
+
+
